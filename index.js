1df14d8b845d8138397e868ffdd2a1768e90c113
Lilly <lillykassner@yahoo.de>
2022-04-12 16:36:27 +0200

    nearly finished without bonus

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [],
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+  
+  this.items.push(item) 
+  this.items.sort(function(a, b){return a-b}) 
+  return this.items
+  }
 
-  get(pos) {}
+  get(pos) {
 
-  max() {}
 
-  min() {}
+if (this.items[pos] <= this.items[this.items.length-1]){
+      return this.items[pos]
+    } 
+    
+    else {
+      throw new Error('OutOfBounds');
+    } 
+  }
+
+  max() {
+
+  if( this.items.length === 0){
+    throw new Error('EmptySortedList');
+  } else {
+
+    return Math.max(...this.items)
+  }
+
+  }
+
+  min() {
+    if( this.items.length === 0){
+      throw new Error('EmptySortedList');
+    } else {
+  
+      return Math.min(...this.items)
+    }
+  }
 
   sum() {}
 
